bf74964199b1c86b0625fdac857bcc8e3e2b3b84
DaviGGA <davigregorio.a@hotmail.com>
2022-10-25 14:55:55 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,89 @@
+const form = document.getElementById("novoItem")
+const lista = document.getElementById('lista')
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach((element) => {
+    criaElemento(element)
+})
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    let nome = event.target.elements['nome']
+    let quantidade = event.target.elements['quantidade']
+
+    const existe = itens.find(elemento => elemento.nome == nome.value)
+
+    const itemAtual = {
+        "nome" : nome.value,
+        "quantidade" : quantidade.value
+    }
+    
+    if (existe) {  
+        
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+       
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+   
+    } else {
+        
+        itemAtual.id = itens[itens.length - 1] ?  (itens[itens.length - 1]).id + 1: 0;
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+        
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+    nome.value = ""
+    quantidade.value = ""
+
+})
+
+function criaElemento(item) {
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+    
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += item.nome
+    novoItem.appendChild(botaoDeleta(item.id))
+
+    lista.appendChild(novoItem)
+
+
+
+}
+
+function atualizaElemento(item) {
+
+    let itemAtualiza = document.querySelector(["[data-id='" + item.id + "']"])
+    itemAtualiza.innerHTML = item.quantidade
+
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerHTML = "X"
+
+    elementoBotao.addEventListener("click", function () {
+        
+        let listaParente = (this.parentElement)
+        deletaElemento(listaParente, id)
+        
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag,id) {
+    console.log(id)
+    tag.remove()
+    itens.splice(itens.findIndex(elemento => elemento.id == id),1)
+    localStorage.setItem("itens", JSON.stringify(itens))
+}
